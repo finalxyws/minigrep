60f8807136356959425cd704c916e3f927012686
finalxyws <finalxyws@gmail.com>
2023-11-03 14:14:31 +0800

    Print errors to standard error

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(args.as_slice()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
