9fe2aea4f4b1d38dab94e6262bcd5ffbacfba2ff
finalxyws <finalxyws@gmail.com>
2023-11-03 10:31:08 +0800

    Extract parse config functionality from main

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query_string = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
-    println!("Searching for {}", query_string);
+    println!("Searching for {}", query);
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
