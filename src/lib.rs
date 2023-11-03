309afce94ecab3ce169329cc3c8d564a6bf7fbde
finalxyws <finalxyws@gmail.com>
2023-11-03 12:11:25 +0800

    Finish search function

--- src/lib.rs
+++ src/lib.rs
@@ -66,15 +66,14 @@ Pick three.";
     }
 
     pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-        // let mut results = Vec::new();
+        let mut results = Vec::new();
 
-        // for line in contents.lines() {
-        //     if line.contains(query) {
-        //         results.push(line);
-        //     }
-        // }
+        for line in contents.lines() {
+            if line.contains(query) {
+                results.push(line);
+            }
+        }
 
-        // results
-        vec![]
+        results
     }
 }
