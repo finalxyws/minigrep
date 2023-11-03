183d115376d44fa8d613ceacbcbb5596ad006c02
finalxyws <finalxyws@gmail.com>
2023-11-03 12:06:42 +0800

    Writing failing tests

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,60 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{}", contents);
-
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn config_build() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+            String::from("file_path"),
+        ];
+
+        let config = Config::build(&args).unwrap();
+
+        assert_eq!(config.query, "query");
+        assert_eq!(config.file_path, "file_path");
+    }
+
+    #[test]
+    fn config_build_not_enough_args() {
+        let args = vec![
+            String::from("minigrep"),
+            String::from("query"),
+        ];
+
+        let config = Config::build(&args);
+
+        assert!(config.is_err());
+    }
+
+    pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        // let mut results = Vec::new();
+
+        // for line in contents.lines() {
+        //     if line.contains(query) {
+        //         results.push(line);
+        //     }
+        // }
+
+        // results
+        vec![]
+    }
 }
